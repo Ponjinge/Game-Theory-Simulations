5d38ec58d95013bc9c862df0006e8c7bea4781b1
Ponjinge <34633743+Ponjinge@users.noreply.github.com>
2024-02-13 15:31:26 +0100

    Create player_template.py
    Template for making new players

---
+++ Prisoners_dilemma/players/player_template.py
@@ -0,0 +1,31 @@
+class ClassName:
+    def __init__(self):
+        pass
+    
+    def decide(self, previous_choices = [], position = 0):
+        
+        # Position indicates whether you are the first or second player
+        # This is used for determining what your or your opponent's last move was
+        
+        if position == 0:
+            k = 1 #Opponent pos
+        else:
+            k = 0 #Opponent pos
+        
+        # If first round
+        if previous_choices == []:
+            return 1 # Defect
+        
+        # If you cooperated last time
+        if previous_choices[-1][position] == 0 :
+            return 0 # Cooperate
+        
+        # If your opponent defected last time
+        elif previous_choices[-1][k] == 0:
+            
+            return 0 # Cooperate
+        
+        else:
+            return 1 # Defect
+        
+        

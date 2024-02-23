16c349f64625071e742ff26df5c30e70b0a80c42
ascii-digital <gabrielsaunders44@gmail.com>
2024-02-23 02:49:41 +0000

    Added string methods for players

--- Prisoners_dilemma/main.py
+++ Prisoners_dilemma/main.py
@@ -4,14 +4,14 @@ from players.red import Red
 from players.green import Green 
 from players.tit_for_tat import TitForTat
 from players.nasty import Nasty
-
+from players.random import Random
         
 G = Green()
 R = Red()
 T4T = TitForTat()
 N = Nasty()
-
-player_list = [G,R,T4T,N]
+Rand = Random()
+player_list = [G,R,T4T,N,Rand]
 
 # Single game test
 print(prisoner_dilemma.game(G,R,10)) # expected output: [0, 50]
@@ -25,8 +25,7 @@ print(prisoner_dilemma.p_set(player_list, 100))
 # Test a simple tournament
 prisoner_dilemma.tournament(player_list, 5, 250)
 
-print(prisoner_dilemma.evolvelist(player_list, 250, 3, 4, 'top_bottom', 2))
+print(prisoner_dilemma.evolvelist(player_list, 200, 3, 500, 'top_bottom', 2))
 
 
 
-print(G)
--- Prisoners_dilemma/players/red.py
+++ Prisoners_dilemma/players/red.py
@@ -2,6 +2,14 @@ class Red :
     
     def __init__(self):
         pass
+
+    def __str__(self):
+        return self.__class__.__name__
+    
+    def __repr__(self):
+        return self.__class__.__name__
+
+    
     
     def decide(self, previous_choices = [], position = 0):
         return 1 
